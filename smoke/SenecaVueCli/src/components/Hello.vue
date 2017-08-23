e402600003a693c0a8f65869b5eaa631600e3037
davidwroche <davidwroche@yahoo.co.uk>
2017-08-23 13:47:39 +0100

    Imported seneca into vue component with test loading

--- smoke/SenecaVueCli/src/components/Hello.vue
+++ smoke/SenecaVueCli/src/components/Hello.vue
@@ -1,31 +1,31 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <b>first</b><br>
+    <input v-model="msg">
+    <button v-on:click="act">act</button>
+    <button v-on:click="show">show</button>
   </div>
 </template>
 
 <script>
+import seneca from './seneca/seneca.js'
+import senecatest from './seneca/senecatest.js'
+
 export default {
-  name: 'hello',
-  data () {
-    return {
-      msg: 'Welcome to Your MOO.js App Bitchez & Wigroez'
+
+  data: {
+    msg: 'a:1,x:3',
+  },
+  methods: {
+
+    act: function () {
+      seneca.act(this.msg, function(err, out) {
+        this.act({cm:'second', res:out})
+      })
+    },
+
+    show: function () {
+      seneca.act({ann:'show', show:JSON.stringify(this.msg)})
     }
   }
 }
