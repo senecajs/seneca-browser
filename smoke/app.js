cf26881e96e51cb5bbed333ab9c6f705cdc3d362
Georgi Griffiths <georgi.griffiths@gmail.com>
2018-07-11 11:39:46 +0100

    add endpoint option for browser plugin

--- smoke/app.js
+++ smoke/app.js
@@ -1,6 +1,19 @@
 Seneca()
   .test('print')
-  .client({type:'browser', pin:['a:*','b:*','c:*']})
+  .client({ type: 'browser', pin: ['a:*', 'b:*', 'c:*'] })
+
+  // ok
+  .act('a:1,x:1', console.log)
+
+  // unsafe
+  .act('b:1,x:2', console.log)
+
+  // error
+  .act('c:1,x:3', console.log)
+
+Seneca({ plugin: { browser: { endpoint: '/api/seneca' } } })
+  .test('print')
+  .client({ type: 'browser', pin: ['a:*', 'b:*', 'c:*'] })
 
   // ok
   .act('a:1,x:1', console.log)
--- smoke/server.js
+++ smoke/server.js
@@ -2,40 +2,38 @@ var Seneca = require('seneca')
 var Hapi = require('hapi')
 var Inert = require('inert')
 
+var seneca = Seneca({ legacy: { transport: false } })
+  .test('print')
 
-var seneca = Seneca({legacy:{transport:false}})
-    .test('print')
+  .use('handler')
+  .listen({ type: 'browser', pin: ['a:*', 'c:*'] })
 
-    .use('handler')
-    .listen({type:'browser', pin:['a:*','c:*']})
-
-
-seneca.add('a:1', function (msg, reply) {
-  reply({x: 1 + msg.x})
+seneca.add('a:1', function(msg, reply) {
+  reply({ x: 1 + msg.x })
 })
 
-seneca.add('b:1', function (msg, reply) {
-  reply({x: 2 + msg.x})
+seneca.add('b:1', function(msg, reply) {
+  reply({ x: 2 + msg.x })
 })
 
-seneca.add('c:1', function (msg, reply) {
+seneca.add('c:1', function(msg, reply) {
   reply(new Error('foo'))
 })
 
-seneca.ready(function () {
+seneca.ready(function() {
   var handler = seneca.export('handler')
 
   var server = new Hapi.Server()
-  server.connection({port: 8080})
+  server.connection({ port: 8080 })
 
-  server.register( Inert )
+  server.register(Inert)
 
   server.route({
     method: 'GET',
     path: '/{path*}',
     handler: {
       directory: {
-        path: __dirname,
+        path: __dirname
       }
     }
   })
@@ -48,14 +46,21 @@ seneca.ready(function () {
     }
   })
 
-  server.route({ 
-    method: 'POST', path: '/seneca', 
-    handler: function( request, reply ) {
+  server.route({
+    method: 'POST',
+    path: '/seneca',
+    handler: function(request, reply) {
       handler(request.payload, reply)
     }
   })
 
-
+  server.route({
+    method: 'POST',
+    path: '/api/seneca',
+    handler: function(request, reply) {
+      handler(request.payload, reply)
+    }
+  })
 
   server.start(console.log)
 })
