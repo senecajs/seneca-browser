a4625bf21b10a93fe054d46901621b26027ef5d4
rjrodger <richard@ricebridge.com>
2021-09-03 20:03:07 +0100

    better header handling

--- seneca-browser-src.js
+++ seneca-browser-src.js
@@ -5,7 +5,7 @@ var Timers = require('timers')
 var SenecaModule = require('seneca')
 var SenecaPromisify = require('seneca-promisify')
 
-console.log('SP', SenecaPromisify)
+console.log('SenecaBrowser 04')
 
 global.setImmediate = global.setImmediate || Timers.setImmediate
 
@@ -21,6 +21,9 @@ var SenecaExport = function (options, more_options) {
     name: 'browser',
     init: function browser(options) {
       options.endpoint = options.endpoint || '/seneca'
+      options.fetch = options.fetch || {}
+      options.headers = options.headers || {}
+
       this.add('role:transport,hook:client,type:browser', hook_client_browser)
 
       var tu = this.export('transport/utils')
@@ -29,24 +32,41 @@ var SenecaExport = function (options, more_options) {
         var seneca = this
 
         reply({
-          send: function (msg, reply, meta) {
-            fetch(options.endpoint, {
-              // TODO: set content-type header to json
-              credentials: 'same-origin',
+          send: async function (msg, reply, meta) {
+            let config = {
+              // credentials: 'same-origin',
               method: 'post',
+              ...options.fetch,
+              mode: 'cors',
+              cache: 'no-cache',
+              headers: await resolveHeaders({
+                'Content-Type': 'application/json',
+                ...options.fetch.headers,
+                ...options.headers,
+              }),
               body: tu.stringifyJSON(tu.externalize_msg(seneca, msg, meta)),
-            })
+            }
+            // console.log('FETCH', options.endpoint, JSON.stringify(config))
+            fetch(options.endpoint, config)
               .then(function (response) {
                 if (response.ok) {
                   return response.json()
                 } else {
                   // FIX: handle transport errors
-                  return null
+                  // return null
+                  return reply(new Error(JSON.stringify(response)))
                 }
               })
               .then(function (json) {
                 // FIX: seneca.reply broken in browser
+
+                // TODO: transport should handle this
+                if(Array.isArray(json)) {
+                  json.meta$ = {id:'ID'}
+                }
+                console.log('REPLY JSON', json)
                 var rep = tu.internalize_reply(seneca, json)
+                console.log('REPLY INT', rep)
                 reply(rep.err, rep.out, rep.meta)
               })
           },
@@ -55,6 +75,23 @@ var SenecaExport = function (options, more_options) {
     },
   })
 
+  
+  async function resolveHeaders(headers) {
+    let names = Object.keys(headers)
+    for(let h of names) {
+      let v = headers[h]
+      if('function'===typeof(v)) {
+        headers[h] = await v()
+      }
+      else {
+        headers[h] = v
+      }
+    }
+    console.log('RH', headers)
+    return headers
+  }
+
+  
   return seneca
 }
 
